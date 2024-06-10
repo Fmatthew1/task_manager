172d243a77651f4f024d329c1f2789953ffa781f
Matthew Fekumoh <mmattez2009@gmail.com>
2024-06-10 11:15:55 +0100

    Added the updated sql file

--- task_manager.sql
+++ task_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 19, 2024 at 01:40 PM
+-- Generation Time: Jun 10, 2024 at 10:13 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `todos` (
   `id` int UNSIGNED NOT NULL,
+  `user_id` int DEFAULT NULL,
   `name` varchar(100) NOT NULL,
   `is_completed` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -40,26 +41,40 @@ CREATE TABLE `todos` (
 -- Dumping data for table `todos`
 --
 
-INSERT INTO `todos` (`id`, `name`, `is_completed`, `created_at`, `updated_at`, `completed_at`) VALUES
-(1, 'Read GoPro every morning by 4am', 1, '2024-04-16 21:27:00', '2024-04-18 14:29:42', '2024-04-17 07:16:32'),
-(2, 'morning Worship by 6am', 1, '2024-04-16 21:28:42', '2024-04-16 21:28:42', '2024-04-17 07:16:34'),
-(3, 'Start Laravel Project by 9am ', 1, '2024-04-16 21:30:29', '2024-04-16 21:30:29', '2024-04-18 14:31:46'),
-(4, 'Learn PHP OOP from 11am everyday', 0, '2024-04-16 21:31:33', '2024-04-16 21:31:33', '2024-04-16 21:31:33'),
-(5, 'Study JavaScript by 1pm daily', 0, '2024-04-16 21:33:00', '2024-04-16 21:33:00', '2024-04-16 21:33:00'),
-(6, 'Create video Content by 2pm daily', 0, '2024-04-16 21:33:53', '2024-04-16 21:33:53', '2024-04-16 21:33:53'),
-(7, 'Upload content on YouTube by 4pm daily', 0, '2024-04-16 21:34:56', '2024-04-16 21:34:56', '2024-04-16 21:34:56'),
-(8, 'book 10 people for presentation by 5pm daily', 0, '2024-04-16 21:36:35', '2024-04-16 21:36:35', '2024-04-16 21:36:35'),
-(9, 'Follow up 5 prospects by 6pm daily', 0, '2024-04-16 21:37:17', '2024-04-16 21:37:17', '2024-04-16 21:37:17'),
-(10, 'call 3 serious team by 7:30pm daily', 1, '2024-04-16 21:37:57', '2024-04-16 21:38:32', '2024-04-16 21:39:30'),
-(11, 'Personal Development by 4:30pm Daily', 0, '2024-04-18 13:28:44', '2024-04-18 14:29:28', '2024-04-18 13:28:44'),
-(12, 'Practice SAVERS from 5am Daily', 0, '2024-04-19 09:48:24', '2024-04-19 09:48:24', '2024-04-19 09:48:24'),
-(13, 'Do 30 press ups every morning', 0, '2024-04-19 10:55:00', '2024-04-19 10:55:00', '2024-04-19 10:55:00'),
-(14, 'Do 30 push ups every morning', 0, '2024-04-19 10:58:50', '2024-04-19 10:58:50', '2024-04-19 10:58:50'),
-(15, 'Jog every morning', 0, '2024-04-19 11:08:06', '2024-04-19 11:08:06', '2024-04-19 11:08:06'),
-(16, 'Brush your teeth by 7am every morning', 0, '2024-04-19 11:11:14', '2024-04-19 11:11:14', '2024-04-19 11:11:14'),
-(17, 'Eat Breakfast by 7:30am', 1, '2024-04-19 11:27:10', '2024-04-19 11:27:10', '2024-04-19 11:29:05'),
-(18, 'Go the office by 8am', 1, '2024-04-19 11:28:39', '2024-04-19 11:28:39', '2024-04-19 11:29:42'),
-(19, 'Buy shoe for Chrysolite tomorrow', 0, '2024-04-19 11:35:45', '2024-04-19 11:35:45', NULL);
+INSERT INTO `todos` (`id`, `user_id`, `name`, `is_completed`, `created_at`, `updated_at`, `completed_at`) VALUES
+(1, 2, 'implement a multitenant system', 1, '2024-06-08 15:16:16', '2024-06-08 15:16:16', '2024-06-08 15:16:41'),
+(2, 2, 'dispose the refuse', 0, '2024-06-08 15:16:36', '2024-06-08 15:16:36', NULL),
+(3, 10, 'Buy 2kg of Gas', 0, '2024-06-08 15:38:14', '2024-06-08 15:38:14', NULL),
+(4, 11, 'Learn video Editing', 0, '2024-06-08 15:43:28', '2024-06-08 15:43:28', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` enum('active','inactive') DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Mattez', 'matfeks@gmail.com', 'inactive', '2024-06-08 08:56:27', '2024-06-08 15:57:24'),
+(2, 'Tobi', 'tobi@gmail.com', 'inactive', '2024-06-08 15:32:30', '2024-06-08 17:14:02'),
+(6, 'Ayo', 'ayo@gmail.com', 'active', '2024-06-08 15:48:04', '2024-06-08 17:13:27'),
+(7, 'David', 'david@gmail.com', 'inactive', '2024-06-08 15:52:21', '2024-06-08 17:12:49'),
+(8, 'John Mikes', 'johnmikes@gmail.com', 'inactive', '2024-06-08 15:54:50', '2024-06-08 15:57:18'),
+(9, 'jane doe', 'jane@gmail.com', 'active', '2024-06-08 16:17:57', '2024-06-08 16:19:20'),
+(10, 'Christal', 'christal@gmail.com', 'active', '2024-06-08 16:36:09', '2024-06-08 16:36:09'),
+(11, 'Derek', 'derek@gmail.com', 'active', '2024-06-08 16:42:06', '2024-06-08 16:42:06');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +84,15 @@ INSERT INTO `todos` (`id`, `name`, `is_completed`, `created_at`, `updated_at`, `
 -- Indexes for table `todos`
 --
 ALTER TABLE `todos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -79,7 +102,23 @@ ALTER TABLE `todos`
 -- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `todos`
+--
+ALTER TABLE `todos`
+  ADD CONSTRAINT `todos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
