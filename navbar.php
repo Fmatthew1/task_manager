64427c12fc56db27d8736d2e7ab91c3df5ea6c93
Matthew Fekumoh <mmattez2009@gmail.com>
2024-06-10 17:40:25 +0100

    created the nav bar

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!-- navbar.php -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Task Manager</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <button class="btn btn-sm btn-outline-success me-2" type="button"><a class="nav-link" href="index.php">Todos</a></button>
+                </li>
+                <li class="nav-item">
+                <button class="btn btn-sm btn-outline-success" type="button"><a class="nav-link" href="home.php">Users</a></button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
