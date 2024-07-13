4fad46c99b4a08624cd19aa23670a7d2b700df5b
Matthew Fekumoh <mmattez2009@gmail.com>
2024-07-13 17:18:12 +0100

    created to handle toggle user

---
+++ toggle_user.php
@@ -0,0 +1,22 @@
+<?php
+include 'Db.php';
+include 'users.php';
+
+$id = $_GET['id'] ?? null;
+
+if (!$id) {
+    die("User ID is required");
+}
+
+$user = User::find($conn, $id);
+
+if (!$user) {
+    die("User not found");
+}
+
+if ($user->toggleStatus()) {
+    header("Location: manage_users.php");
+    exit();
+} else {
+    die("Error updating user status.");
+}
