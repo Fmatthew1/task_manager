9cfe29a40e9cf3bcfff39c1c277ed340d757aa58
Matthew Fekumoh <mmattez2009@gmail.com>
2024-07-23 16:05:27 +0100

    access denied page

---
+++ unauthorized.php
@@ -0,0 +1,21 @@
+<php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Unauthorized</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Access Denied</h1>
+        <p>You do not have permission to access this page.</p>
+    </div>
+</body>
+</html>
+
+
+
+
